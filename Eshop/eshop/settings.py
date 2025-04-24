11c753ef254ffecb880db169ef37a47a5fddc5fa
andrej1991banas <limitless.lokca@gmail.com>
2025-04-24 15:38:13 +0200

    Update settings.py

--- Eshop/eshop/settings.py
+++ Eshop/eshop/settings.py
@@ -31,7 +31,7 @@ load_dotenv()
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS =config('ALLOWED_HOSTS', cast=lambda v: [s.strip() for s in v.split(',')])
 
