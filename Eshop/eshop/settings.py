1751941103d196e03df66683d7a30b2d0a540089
andrej1991banas <limitless.lokca@gmail.com>
2025-04-25 09:19:01 +0200

    .

--- Eshop/eshop/settings.py
+++ Eshop/eshop/settings.py
@@ -31,9 +31,9 @@ load_dotenv()
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS =config('ALLOWED_HOSTS', cast=lambda v: [s.strip() for s in v.split(',')])
+ALLOWED_HOSTS =['your-domain.com', 'Flyfishing-Django-Eccommerce.ondigitalocean.app', '127.0.0.1']
 
 
 # Application definition
